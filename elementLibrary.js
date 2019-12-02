2b1f6dd5611ade686963aab1c7a1a86ab7bb9e0b
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-12-01 17:23:12 -0700

    dropdown & checkboxes created

--- elementLibrary.js
+++ elementLibrary.js
@@ -22,7 +22,7 @@ customElements.define('e-input', class tfeStd extends tfe {
 
     get css(){
         return (
-            `button { 
+            `button{ 
                 width: 100px;
                 height: 31px;
                 border: .7px solid #505050; 
@@ -40,7 +40,7 @@ customElements.define('e-input', class tfeStd extends tfe {
                 border: .7px solid #009B00;
             }
 
-            input {
+            input{
                 width: 200px;
                 height: 20px;
                 border: .7px solid #505050; 
@@ -48,7 +48,7 @@ customElements.define('e-input', class tfeStd extends tfe {
                 padding: 5px;
             }
 
-            h3 {
+            h3{
                 font-family: Roboto, sans serif;
                 font-weight: 700;
                 font-size: 12px;
@@ -82,7 +82,7 @@ customElements.define('multi-input', class tfeStd extends tfe {
 
     get css(){
         return (
-            `button { 
+            `button{ 
                 width: 100px;
                 height: 31px;
                 border: .7px solid #505050; 
@@ -101,7 +101,7 @@ customElements.define('multi-input', class tfeStd extends tfe {
                 border: .7px solid #009B00;
             }
 
-            textarea {
+            textarea{
                 width: 300px;
                 height: 200px;
                 border: .7px solid #505050; 
@@ -110,7 +110,7 @@ customElements.define('multi-input', class tfeStd extends tfe {
                 display: block;
             }
 
-            h3 {
+            h3{
                 font-family: Roboto, sans serif;
                 font-weight: 700;
                 font-size: 12px;
@@ -129,6 +129,168 @@ customElements.define('multi-input', class tfeStd extends tfe {
         )
     }
 })
+
+customElements.define('dropdown', class tfeStd extends tfe {
+    
+    constructor(){
+        super();
+    }
+
+    get css(){
+        return (
+            `button{ 
+                width: 100px;
+                height: 31px;
+                border: .7px solid #505050; 
+                border-radius: 2px;
+                padding: 5px;
+                font-family: Roboto, sans serif;
+                font-weight: 700;
+                font-size: 12px;
+                letter-spacing: .6px;
+                margin-top: 17px;
+            }
+
+            button:hover{
+                background-color: #009B00;
+                color: #FFFFFF;
+                border: .7px solid #009B00;
+            }
+
+            select{
+                width: 300px;
+                height: 200px;
+                border: .7px solid #505050; 
+                border-radius: 2px;
+                padding: 5px;
+            }
+
+            h3{
+                font-family: Roboto, sans serif;
+                font-weight: 700;
+                font-size: 12px;
+                letter-spacing: .6px;
+            }`
+        )
+    }
+
+    get html(){
+        return (
+            `<h3> CHOOSE SOMETHING </h3>
+            <select name="animals">
+                <option value="dog">Dog</option>
+                <option value="billy-goat">Billy Goat</option>
+                <option value="jaguar">Jaguar</option>
+            </select>
+            <button>SUBMIT</button>`
+        )
+    }
+})
+
+customElements.define('checkboxes', class tfeStd extends tfe {
+    
+    constructor(){
+        super();
+    }
+
+    get css(){
+        return (
+            `button{ 
+                width: 100px;
+                height: 31px;
+                border: .7px solid #505050; 
+                border-radius: 2px;
+                padding: 5px;
+                font-family: Roboto, sans serif;
+                font-weight: 700;
+                font-size: 12px;
+                letter-spacing: .6px;
+                margin-top: 17px;
+            }
+
+            button:hover{
+                background-color: #009B00;
+                color: #FFFFFF;
+                border: .7px solid #009B00;
+            }
+
+            input{
+                width: 200px;
+                height: 20px;
+                border: .7px solid #505050; 
+                border-radius: 2px;
+                padding: 5px;
+            }
+
+            h3{
+                font-family: Roboto, sans serif;
+                font-weight: 700;
+                font-size: 12px;
+                letter-spacing: .6px;
+            }`
+        )
+    }
+
+    get html(){
+        return (
+            `<h3> CHOOSE FOOD </h3>
+            <input type="checkbox" name="potato" value="Potato"> Potato </input>
+            <input type="checkbox" name="pizza" value="Pizza"> Pizza </input>
+            <input type="checkbox" name="turkey" value="Turkey"> Turkey </input>
+            <button>SUBMIT</button>`
+        )
+    }
+})
+
+customElements.define('navigation', class tfeStd extends tfe {
+    
+    constructor(){
+        super();
+    }
+
+    get css(){
+        return (
+            `li{
+                background-color: #505050;
+                font-family: Roboto, sans serif;
+                font-weight: 700;
+                font-size: 12px;
+            }
+
+            li:hover{
+                background-color: #009B00;
+                color: #FFFFFF;
+            }
+
+            h3{
+                font-family: Roboto, sans serif;
+                font-weight: 700;
+                font-size: 12px;
+                letter-spacing: .6px;
+            }
+            
+            nav{
+                display: flex;
+                justify-content: space-between;
+            }`
+        )
+    }
+
+    get html(){
+        //switch H1 below to an img link once i get access to BPX logo
+        return (
+            `<nav>
+                <h1> BPX <h1> 
+                <ul>
+                    <li> Link1 </li>
+                    <li> Link2 </li>
+                    <li> Link3 </li>
+                </ul>
+            </nav>`
+        )
+    }
+})
+
 customElements.define('e-table', class tfeTable extends tfe {
     
     constructor(){
